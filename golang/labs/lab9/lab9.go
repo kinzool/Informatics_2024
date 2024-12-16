d9984533e78def6df94b0062377d421d33763b34
kinzool <solovyevkirill2007@mail.ru>
2024-12-16 22:57:18 +0300

    Second version of lab9

--- golang/labs/lab9/lab9.go
+++ golang/labs/lab9/lab9.go
@@ -1,8 +1,14 @@
 package lab9
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 const filePath = "../golang/labs/lab9/"
+
+var command string
 
 var filename string
 
@@ -24,41 +30,71 @@ func RunLab9() {
 		fmt.Println(err)
 		return
 	}
-
+	reader := bufio.NewReader(os.Stdin)
 	TaskList := ToDoList{}
 
 	err = ReadDataFromFile(name, &TaskList)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	err = TaskList.AddTask("Сделать 9 лабу", name)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = TaskList.AddTask("Проснуться к первой паре", name)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = TaskList.DeleteTask(1, name)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	taskName, err := TaskList.SearchTask("Сделать")
-	if err == nil {
-		fmt.Println(taskName)
-	}
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	TaskList.PrintToDoList()
-	err = WriteDataToFile(name, &TaskList)
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println("Для дальнейшего действия введите одну из приведенных ниже команд:\n" +
+		"1. Добавить задачу\n" +
+		"2. Показать все задачи\n" +
+		"3. Отметить задачу как выполненную\n" +
+		"4. Удалить задачу\n" +
+		"5. Поиск задачи\n" +
+		"6. Выйти")
+	for {
+		fmt.Println("Ожидание ввода команды:")
+		fmt.Scan(&command)
+		switch command {
+		case "1":
+			fmt.Println("Введите описание задачи:")
+			_, _ = reader.ReadString('\n')
+			newTask, _ := reader.ReadString('\n')
+			newTask = newTask[:len(newTask)-2]
+			err = TaskList.AddTask(newTask, name)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Задача '%s' добавлена\n", newTask)
+		case "2":
+			TaskList.PrintToDoList()
+		case "3":
+			fmt.Println("Введите ключевое слово для поиска задачи, которую хотите пометить как выполненную:")
+			var description string
+			fmt.Scan(&description)
+			taskName, err := TaskList.MarkAsComplete(description, name)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Задача '%s' выполнена\n", taskName)
+		case "4":
+			fmt.Println("Введите индекс задачи, которую вы хотите удалить:")
+			var index int
+			fmt.Scan(&index)
+			fmt.Println(index)
+			taskName, err := TaskList.DeleteTask(index, name)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Задача '%s' удалена\n", taskName)
+		case "5":
+			fmt.Println("Введите ключевое слово для поиска задачи:")
+			var taskName string
+			fmt.Scanln(&taskName)
+			searchedTask, err := TaskList.SearchTask(taskName)
+			if err == nil {
+				fmt.Println(searchedTask)
+			}
+		case "6":
+			fmt.Println("Работа программы завершена!")
+			return
+		default:
+			fmt.Println("Неверно введена команда")
+		}
 	}
 }
--- golang/labs/lab9/workwithtodolist.go
+++ golang/labs/lab9/workwithtodolist.go
@@ -18,24 +18,25 @@ func (l *ToDoList) AddTask(description string, name string) error {
 	return nil
 }
 
-func (l *ToDoList) DeleteTask(index int, name string) error {
+func (l *ToDoList) DeleteTask(index int, name string) (string, error) {
 	if index >= len(l.Tasks) || index < 0 {
-		return fmt.Errorf("удаление задачи: индекс %d вне диапазона", index)
+		return "", fmt.Errorf("удаление задачи: индекс %d вне диапазона", index)
 	}
+	output := l.Tasks[index].Description
 	l.Tasks = append(l.Tasks[:index], l.Tasks[index+1:]...)
 	WriteDataToFile(name, l)
-	return nil
+	return output, nil
 }
 
-func (l *ToDoList) MarkAsComplete(description string, name string) error {
+func (l *ToDoList) MarkAsComplete(description string, name string) (string, error) {
 	for i := range l.Tasks {
 		if strings.Contains(l.Tasks[i].Description, description) {
 			l.Tasks[i].Status = "выполнена"
-			return nil
+			return l.Tasks[i].Description, nil
 		}
 	}
 	WriteDataToFile(name, l)
-	return fmt.Errorf("изменение статуса задачи: задачи, соответсвующей описанию, не существует")
+	return "", fmt.Errorf("изменение статуса задачи: задачи, соответсвующей описанию, не существует")
 }
 
 func (l *ToDoList) SearchTask(keyword string) (string, error) {
@@ -43,15 +44,18 @@ func (l *ToDoList) SearchTask(keyword string) (string, error) {
 		if strings.Contains(l.Tasks[i].Description, keyword) {
 			taskStr, err := json.Marshal(l.Tasks[i])
 			if err != nil {
-				return "", fmt.Errorf("поиск задачи, %w", err)
+				return "", fmt.Errorf("поиск задачи: %w", err)
 			}
 			return string(taskStr), nil
 		}
 	}
-	return "", fmt.Errorf("поиск задачи, данная задача не найдена")
+	return "", fmt.Errorf("поиск задачи: данная задача не найдена")
 }
 
 func (l *ToDoList) PrintToDoList() {
+	if len(l.Tasks) == 0 {
+		fmt.Println("Нет добавленных задач")
+	}
 	for i, task := range l.Tasks {
 		fmt.Printf("Номер задачи: %d, Описание задачи: %s, Статус задачи: %s\n", i+1, task.Description, task.Status)
 	}
